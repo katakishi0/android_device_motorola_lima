50e3365e08a9f33b508798bf5e0519ec35a0dec9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-06-16 07:24:23 +0200

    lima: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm Manufacturer: motorola Model: moto g(8) play Script version: 3.0.0

---
+++ omni_lima.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit from lima device
+$(call inherit-product, device/motorola/lima/device.mk)
+
+PRODUCT_DEVICE := lima
+PRODUCT_NAME := omni_lima
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g(8) play
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lima_32_retail-user 10 QMDS30.47-33-5 7e278 release-keys"
+
+BUILD_FINGERPRINT := motorola/lima_32_retail/lima:10/QMDS30.47-33-5/7e278:user/release-keys
