3975e8b0c13a8d44fadd03b0f95d3874bd4eee15
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-07-31 16:45:50 -0300

    Update device.mk

--- device.mk
+++ device.mk
@@ -18,11 +18,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-impl \
     android.hardware.boot@1.0-service
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6771
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6771 \
     libgptutils \
     libz \
     libcutils
