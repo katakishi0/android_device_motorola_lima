d4fcd00a9b04861c76c594fadaca590403bbd4fd
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-08-10 12:04:42 -0300

    rebase

--- README.md
+++ README.md
@@ -1,10 +1,40 @@
-# Android device tree for motorola moto g(8) play (lima)
+# TWRP device tree for Motorola One Macro & G8 Play
 
-```
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-```
+<p align="center">
+  <img height="600" src="/release-date.jpg?raw=true">
+</p>
+
+Basic        | Spec Sheet
+------------:|:-------------------------
+CPU          | Octa-core processor > 4x2.0 Ghz Cortex-A73 & 4x2.0 GHz Cortex-A53
+Chipset      | MediaTek Helio P70
+GPU          | Mali-G72 MP3
+Memory/RAM   | 4 GB
+Storage      | 64 GB
+Battery      | 4000 mAh
+Dimensions   | 157.6mm x 75.4mm x 9 mm
+Display      | 6.2” HD+ IPS Display 90 hz (720X1520)
+Rear Camera  | 13 MP - (wide), 2 MP - (macro), 2 MP - (depth), PDAF
+Front Camera | 8 MP
+Release Date | October 2019
+
+## Build instructions
+- Initialize the source tree -
+  ```bash
+  repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
+  ```
+  You may optionally pass `--depth=1` to save space, like so:
+  ```bash
+  repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0
+  ```
+- Clone this repository -
+  ```bash
+  git clone https://github.com/mototek/recovery_motorola_lima device/motorola/lima
+  ```
+- Build -
+  ```bash
+  source build/envsetup.sh
+  export ALLOW_MISSING_DEPENDENCIES=true
+  lunch omni_lima-eng
+  mka recoveryimage
+  ```
--- omni_lima.mk
+++ omni_lima.mk
@@ -1,28 +1,36 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+# Copyright (C) 2020 - 2021 Doritos™ Motorola | Developers
+# Copyright (C) 2019 - 2021 Motorola™ MT67XX | MotoTek
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Release name
+PRODUCT_RELEASE_NAME := Motorola g8 play
 
-# Inherit some common Omni stuff.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from lima device
-$(call inherit-product, device/motorola/lima/device.mk)
-
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := lima
 PRODUCT_NAME := omni_lima
-PRODUCT_BRAND := motorola
-PRODUCT_MODEL := moto g(8) play
-PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := Motorola
+PRODUCT_MODEL := moto g8 play
+PRODUCT_MANUFACTURER := Motorola
 
-PRODUCT_GMS_CLIENTID_BASE := android-motorola
+# HACK: Set vendor patch level
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.build.security_patch=2099-12-31
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="lima_32_retail-user 10 QMDS30.47-33-5 7e278 release-keys"
-
-BUILD_FINGERPRINT := motorola/lima_32_retail/lima:10/QMDS30.47-33-5/7e278:user/release-keys
